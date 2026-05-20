e8384a6979027f0aa0408fcd151bd66ce45ef7dd
Faisal N <contact@faisaln.com>
2026-05-20 14:10:27 -0400

    Change website notices from warnings to info Fixes #2

--- global.js
+++ global.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('.alert-badge i')) document.querySelector('.alert-badge i').className = "fa-solid fa-bell";
-    if (document.querySelector('.ss-alert-modal-svg-container')) document.querySelector('.ss-alert-modal-svg-container').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
+    document.querySelector('.alert-badge')?.addEventListener('click', () => {
+        var changeIconInterval = setInterval(() => {
+            if (document.querySelector('.ss-alert-modal-svg-container')) {
+                document.querySelector('.ss-alert-modal-svg-container').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
+                if (document.querySelector('.ss-alert-modal-cta-button') && (document.querySelector('.ss-alert-modal-cta-button').href === window.location.href)) document.querySelector('.ss-alert-modal-cta-button').remove();
+                clearInterval(changeIconInterval);
+                setTimeout(() => {
+                    document.querySelector('.alert-badge').classList.add('active');
+                }, 100);
+            };
+        }, 500);
+    });
     /* Array.from($0.querySelectorAll('img')).map(img => {
         img.style.height = 'fit-content';
         img.style.maxHeight = '75px';
