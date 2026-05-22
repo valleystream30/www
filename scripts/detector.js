f41e75d5373b910e62c2c30df7c70f1900b6f00c
Faisal N <contact@faisaln.com>
2026-05-21 23:13:42 -0400

    Add home page link detection to crawler

--- scripts/detector.js
+++ scripts/detector.js
@@ -2,6 +2,7 @@
 
 var sitemap = Array.from(document.querySelectorAll('span')).filter(link => !link.classList.length && !link.id && link.innerText.startsWith('http')).map(link => link.innerText);
 var linksWithBackTo = [];
+var linksWithSectionHome = [];
 var linksWithAccordion = [];
 var linksWithError = [];
 var interval = setInterval(() => {
@@ -9,6 +10,7 @@ var interval = setInterval(() => {
         clearInterval(interval);
         console.log('Sitemap crawl complete');
         console.log('Back to:', linksWithBackTo);
+        console.log('Home page links:', linksWithSectionHome);
         console.log('Accordion:', linksWithAccordion);
         console.log('Errors:', linksWithError);
         return;
@@ -16,6 +18,7 @@ var interval = setInterval(() => {
     const link = sitemap.shift();
     fetch(link).then(res => res.text()).then(res => {
         if (res.toLowerCase().includes('back to')) linksWithBackTo.push(link);
+        if (res.toLowerCase().includes('home page') || res.includes(' HOME')) linksWithSectionHome.push(link);
         if (res.toLowerCase().includes('ss-accordion-heading')) linksWithAccordion.push(link);
     }).catch(err => {
         console.log('Error on', link);
@@ -26,6 +29,13 @@ function editLinks(array) {
     array.forEach(link => {
         setTimeout(() => {
             window.open(`https://www.valleystream30.com/index.php?pageID=${link.split('/')[3]}&adminArea=1`, '_blank');
+        }, 1000);
+    });
+};
+function openLinks(array) {
+    array.forEach(link => {
+        setTimeout(() => {
+            window.open(link, '_blank');
         }, 1000);
     });
 };
