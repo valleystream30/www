5c11cec90482b62dbe2d4990c7bbeeeb00089050
Faisal N <contact@faisaln.com>
2026-05-22 21:41:05 -0400

    Custom translation popup Fixes #15

--- global.js
+++ global.js
@@ -1,3 +1,15 @@
+function resetTranslate() {
+    var iframe = document.getElementsByClassName('goog-te-banner-frame')[0] || document.getElementById(':1.container');
+    if (!iframe) return;
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var restore_el = innerDoc.getElementsByTagName("button");
+    for (var i = 0; i < restore_el.length; i++) {
+        if (restore_el[i].id.indexOf("restore") >= 0) {
+            restore_el[i].click();
+            return;
+        };
+    };
+};
 function onReady(callback) {
     if (document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', callback);
@@ -6,62 +18,163 @@ function onReady(callback) {
     };
 };
 onReady(() => {
-    if (document.querySelector('.alert-badge i')) document.querySelector('.alert-badge i').className = "fa-solid fa-bell";
-    document.querySelector('.alert-badge')?.addEventListener('click', () => {
-        var changeIconInterval = setInterval(() => {
-            if (document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))')) {
-                document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))').innerHTML = `<svg class="new" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
-                if (document.querySelector('.ss-alert-modal-cta-button') && (document.querySelector('.ss-alert-modal-cta-button').href === window.location.href)) document.querySelector('.ss-alert-modal-cta-button').remove();
-                setTimeout(() => {
-                    document.querySelector('.alert-badge').classList.add('active');
-                }, 100);
-            };
-            if (!document.querySelector('.ss-alert-modal-svg-container')) clearInterval(changeIconInterval);
-        }, 500);
-    });
-    /* Array.from($0.querySelectorAll('img')).map(img => {
-        img.style.height = 'fit-content';
-        img.style.maxHeight = '75px';
-        img.style.width = 'auto';
-        return img.outerHTML;
-    }).join('&nbsp;&nbsp;&nbsp;&nbsp;'); */
-    const pageTitle = document.querySelector('title').textContent.toLowerCase();
-    if (pageTitle.includes('partnership')) {
-        document.querySelector('.ss-editor-content p:last-child').classList.add('partnerships');
-    } else if (pageTitle.includes('schools')) {
-        var interval = setInterval(() => {
-            if (document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child a[style]').length === 3) {
-                var minHeight = Array.from(document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child img')).map(img => img.clientHeight).sort()[0];
-                document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child a').forEach(img => {
-                    img.style.height = `${minHeight}px`;
-                    img.style.width = 'unset';
-                    img.style.borderRadius = '10px';
+    try {
+        if (document.querySelector('.alert-badge i')) document.querySelector('.alert-badge i').className = "fa-solid fa-bell";
+        document.querySelector('.alert-badge')?.addEventListener('click', () => {
+            var changeIconInterval = setInterval(() => {
+                if (document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))')) {
+                    document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))').innerHTML = `<svg class="new" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
+                    if (document.querySelector('.ss-alert-modal-cta-button') && (document.querySelector('.ss-alert-modal-cta-button').href === window.location.href)) document.querySelector('.ss-alert-modal-cta-button').remove();
+                    setTimeout(() => {
+                        document.querySelector('.alert-badge').classList.add('active');
+                    }, 100);
+                };
+                if (!document.querySelector('.ss-alert-modal-svg-container')) clearInterval(changeIconInterval);
+            }, 500);
+        });
+        /* Array.from($0.querySelectorAll('img')).map(img => {
+            img.style.height = 'fit-content';
+            img.style.maxHeight = '75px';
+            img.style.width = 'auto';
+            return img.outerHTML;
+        }).join('&nbsp;&nbsp;&nbsp;&nbsp;'); */
+        const pageTitle = document.querySelector('title').textContent.toLowerCase();
+        if (pageTitle.includes('partnership')) {
+            document.querySelector('.ss-editor-content p:last-child').classList.add('partnerships');
+        } else if (pageTitle.includes('schools')) {
+            var schoolsPageInterval = setInterval(() => {
+                if (document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child a[style]').length === 3) {
+                    var minHeight = Array.from(document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child img')).map(img => img.clientHeight).sort()[0];
+                    document.querySelectorAll('main .ss-component-column-wrapper.ss-three-column > :has(img) > :first-child a').forEach(img => {
+                        img.style.height = `${minHeight}px`;
+                        img.style.width = 'unset';
+                        img.style.borderRadius = '10px';
+                    });
+                    clearInterval(schoolsPageInterval);
+                };
+            }, 100);
+        };
+        // document.addEventListener('keydown', (e) => {
+        //     if (e.key === '0') document.documentElement.removeAttribute('font');
+        //     if (e.key === '1') document.documentElement.setAttribute('font', '1');
+        //     if (e.key === '2') document.documentElement.setAttribute('font', '2');
+        //     if (e.key === '3') document.documentElement.setAttribute('font', '3');
+        //     if (e.key === '4') document.documentElement.setAttribute('font', '4');
+        // });
+        document.querySelector('footer a').href = 'https://maps.app.goo.gl/TPDs2TqNehEzxuMY8';
+        document.querySelectorAll('a.btn').forEach(btn => {
+            if (btn.querySelector('.ss-button-icon')) return;
+            const icon = document.createElement('span');
+            icon.setAttribute('aria-hidden', 'true');
+            icon.className = 'ss-button-icon';
+            btn.appendChild(icon);
+        });
+        const bodyObserver = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (node.classList && node.classList.contains('modal-backdrop') && (document.querySelectorAll('.modal-backdrop').length > 1)) node.remove();
                 });
-                clearInterval(interval);
-            };
-        }, 100);
-    };
-    // document.addEventListener('keydown', (e) => {
-    //     if (e.key === '0') document.documentElement.removeAttribute('font');
-    //     if (e.key === '1') document.documentElement.setAttribute('font', '1');
-    //     if (e.key === '2') document.documentElement.setAttribute('font', '2');
-    //     if (e.key === '3') document.documentElement.setAttribute('font', '3');
-    //     if (e.key === '4') document.documentElement.setAttribute('font', '4');
-    // });
-    document.querySelector('footer a').href = 'https://maps.app.goo.gl/TPDs2TqNehEzxuMY8';
-    document.querySelectorAll('a.btn').forEach(btn => {
-        if (btn.querySelector('.ss-button-icon')) return;
-        const icon = document.createElement('span');
-        icon.setAttribute('aria-hidden', 'true');
-        icon.className = 'ss-button-icon';
-        btn.appendChild(icon);
-    });
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            mutation.addedNodes.forEach(node => {
-                if (node.classList && node.classList.contains('modal-backdrop') && (document.querySelectorAll('.modal-backdrop').length > 1)) node.remove();
             });
         });
-    });
-    observer.observe(document.body, { childList: true });
+        bodyObserver.observe(document.body, { childList: true });
+        var languagesInterval = setInterval(() => {
+            if (document.getElementById('GoogleTranslate')?.options.length) {
+                clearInterval(languagesInterval);
+                var languages = Array.from(document.getElementById('GoogleTranslate').options).slice(1).map(option => {
+                    return {
+                        'language': option.innerText,
+                        'code': option.value,
+                    };
+                });
+                languages.push({
+                    'language': 'English',
+                    'code': '',
+                })
+                languages.sort((a, b) => {
+                    if (a.code === '') return -1;
+                    if (b.code === '') return 1;
+                    if (a.code === 'es') return -1;
+                    if (b.code === 'es') return 1;
+                    if (a.code === 'ur') return -1;
+                    if (b.code === 'ur') return 1;
+                    return 0;
+                });
+                document.querySelectorAll('header .translate > a').forEach(translateButton => {
+                    var languageSelector = document.createElement('div');
+                    languageSelector.className = 'languageSelector notranslate';
+                    languageSelector.innerHTML = `<b>Site Language</b><input type="text" id="languageSearch" placeholder="Search languages..." /><div class="languageOptions">${languages.map(lang => `<span data-code="${lang.code}">${lang.language}</span>`).join('')}</div>`;
+                    translateButton.parentElement.appendChild(languageSelector);
+                    translateButton.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        translateButton.parentElement.querySelector('.languageSelector').classList.toggle('active');
+                    });
+                    translateButton.parentElement.querySelector('.languageSelector #languageSearch').addEventListener('input', (event) => {
+                        const searchTerm = event.target.value.toLowerCase();
+                        translateButton.parentElement.querySelectorAll('.languageSelector .languageOptions span').forEach(option => {
+                            option.style.display = option.innerText.toLowerCase().includes(searchTerm) ? 'block' : 'none';
+                        });
+                    });
+                    translateButton.parentElement.querySelectorAll('.languageSelector .languageOptions span').forEach(option => {
+                        if (option.getAttribute('data-code') === '') option.classList.add('active');
+                        option.addEventListener('click', () => {
+                            if (option.classList.contains('active')) return;
+                            option.classList.add('active');
+                            resetTranslate();
+                            if (option.getAttribute('data-code')) {
+                                document.getElementById('GoogleTranslate').value = option.getAttribute('data-code');
+                                document.getElementById('GoogleTranslate').dispatchEvent(new Event('change'));
+                                translateButton.parentElement.querySelector('.languageSelector').classList.remove('active');
+                            };
+                        });
+                    });
+                });
+                document.addEventListener('click', (event) => {
+                    if (!event.target.closest('.languageSelector') && !event.target.closest('.translate')) document.querySelectorAll('.languageSelector').forEach(languageSelector => {
+                        languageSelector.classList.remove('active');
+                    });
+                });
+                const htmlObserver = new MutationObserver(mutations => {
+                    mutations.forEach(mutation => {
+                        if (mutation.attributeName !== 'lang') return;
+                        var langCode = document.documentElement.getAttribute('lang');
+                        var langName = languages.find(lang => lang.code === langCode)?.language || langCode;
+                        console.log(`Language changing to ${langName} (${langCode})`);
+                        document.querySelectorAll('[lang]:not(html), a:not([tabindex])').forEach(el => {
+                            el.style.display = '';
+                        });
+                        if ((langCode !== 'auto') && (langCode !== 'en') && (langCode !== '')) {
+                            var totalFound = [];
+                            document.querySelectorAll('[lang]:not(html)').forEach(el => {
+                                if (el.getAttribute('lang') === langCode) {
+                                    el.style.display = 'none';
+                                } else {
+                                    totalFound.push(el);
+                                };
+                            });
+                            document.querySelectorAll('a:not([tabindex])').forEach(el => {
+                                if (!languages.map(lang => lang.language).some(lang => el.innerText.includes(lang))) return;
+                                if (!el.innerText.includes(langName)) {
+                                    el.style.display = 'none';
+                                } else {
+                                    totalFound.push(el);
+                                };
+                            });
+                            if (totalFound.length === 0) document.querySelectorAll('[lang]:not(html), a:not([tabindex])').forEach(el => {
+                                el.style.display = '';
+                            });
+                        };
+                        document.querySelectorAll('header .translate > a').forEach(translateButton => {
+                            translateButton.parentElement.querySelectorAll('.languageSelector .languageOptions span').forEach(option => {
+                                option.classList.toggle('active', (option.getAttribute('data-code') === langCode) || ((option.getAttribute('data-code') === '') && (langCode === 'en')));
+                            });
+                        });
+                        console.log(`Language changed to ${langName} (${langCode})`);
+                    });
+                });
+                htmlObserver.observe(document.documentElement, { attributes: true });
+            };
+        }, 500);
+    } catch (e) {
+        console.error(e);
+    };
 });
