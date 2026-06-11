7f9876dfa8e357b576d458a21d563c92abaf860a
Faisal N <contact@faisaln.com>
2026-06-11 16:17:43 -0400

    Add detection of clearstream

--- scripts/detector.js
+++ scripts/detector.js
@@ -7,6 +7,7 @@ var linksWithAccordion = [];
 var linksWithError = [];
 var linksWithSUNY = [];
 var linksWithImageWithoutAlt = {};
+var linksWithClearstream = [];
 var interval = setInterval(async () => {
     if (sitemap.length === 0) {
         clearInterval(interval);
@@ -17,20 +18,28 @@ var interval = setInterval(async () => {
         console.log('Errors:', linksWithError);
         console.log('SUNY:', linksWithSUNY);
         console.log('Images without alt text:', linksWithImageWithoutAlt);
+        console.log('Clearstream:', linksWithClearstream);
         return;
     };
     const link = sitemap.shift();
     await fetch(link).then(res => res.text()).then(res => {
-        if (res.toLowerCase().includes('back to')) linksWithBackTo.push(link);
-        if (res.toLowerCase().includes('home page') || res.includes(' HOME')) linksWithSectionHome.push(link);
-        if (res.toLowerCase().includes('ss-accordion-heading')) linksWithAccordion.push(link);
-        if (res.toLowerCase().includes('state university of new york at')) linksWithSUNY.push(link);
+        var lowercase = res.toLowerCase();
+        if (lowercase.includes('back to')) linksWithBackTo.push(link);
+        if (lowercase.includes('home page') || res.includes(' HOME')) linksWithSectionHome.push(link);
+        if (lowercase.includes('ss-accordion-heading')) linksWithAccordion.push(link);
+        if (lowercase.includes('state university of new york at')) linksWithSUNY.push(link);
         var parser = new DOMParser();
         var doc = parser.parseFromString(res, "text/html");
         var images = doc.querySelectorAll('main img:not(.ss-document-icon):not(.rellax):not([alt]), main img[alt=""]:not(.ss-document-icon):not(.rellax)');
         if (images.length) linksWithImageWithoutAlt[link] = Array.from(images).map(img => img.outerHTML);
+        var textPosition = lowercase.indexOf('clearstream');
+        while (textPosition !== -1) {
+            const prevChar = (textPosition > 0) ? lowercase[textPosition - 1] : '';
+            if ((prevChar !== '/') && (prevChar !== '\\')) linksWithClearstream.push(link);
+            textPosition = lowercase.indexOf('clearstream', textPosition + 1);
+        };
     }).catch(err => {
-        console.log('Error on', link);
+        console.err('Error on', link, err);
         linksWithError.push(link);
     });
 }, 2000);
