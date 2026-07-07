720e6c6889ed641e0f935bd693611e5e0c4945eb
Faisal N <contact@faisaln.com>
2026-07-07 13:44:44 -0400

    Alert modal fixes

--- global.js
+++ global.js
@@ -63,6 +63,7 @@ var currentSlug = null;
 var currentSectionTimer = null;
 var intersecting = new Set();
 var languages = [];
+var changeIconInterval = null;
 
 onReady(async () => {
     try {
@@ -138,7 +139,7 @@ onReady(async () => {
         // header.querySelector('.ss-site-header-title-container').replaceWith(newLink);
         if (document.querySelector('.alert-badge i')) document.querySelector('.alert-badge i').className = "fa-solid fa-bell";
         document.querySelector('.alert-badge')?.addEventListener('click', () => {
-            var changeIconInterval = setInterval(() => {
+            changeIconInterval = setInterval(() => {
                 if (document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))')) {
                     document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))').innerHTML = `<svg class="new" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
                     if (document.querySelector('.ss-alert-modal-cta-button') && (document.querySelector('.ss-alert-modal-cta-button').href === window.location.href)) document.querySelector('.ss-alert-modal-cta-button').remove();
@@ -832,10 +833,10 @@ onReady(async () => {
         };
         var adminBarInner = loggedInUser.full_name && document.querySelector('.adminBar') && document.querySelector('.adminBar').lastElementChild;
         adminBarInner?.setAttribute('logged-in-as', loggedInUser.full_name);
-        if (adminBarInner) adminBarInner.innerHTML += `<a aria-label="Reload Page" href="" class="admin-btn1 col-sm-3">
+        if (adminBarInner) adminBarInner.innerHTML += `<a aria-label="Reload Page" href="${window.location.href}" class="admin-btn1 col-sm-3">
             <em class="fa-light fa-fw fa-rotate logout_loggedin_icon psq_bar_icon_xl hidden-lg hidden-md hidden-sm visible-xs-12"></em>
             <em class="fa-light fa-fw fa-rotate logout_loggedin_icon visible-lg-12 visible-md-12 visible-sm-12 hidden-xs"></em>
-            <span class="hidden-xs">Sign Out</span>
+            <span class="hidden-xs">Reload Page</span>
         </a>`;
         for (let link of document.querySelectorAll('a[href]')) {
             if (link.getAttribute('href') === '') {
@@ -1047,6 +1048,16 @@ window.addEventListener('resize', () => {
     } else {
         previousWidth = newWidth;
     };
+    changeIconInterval = setInterval(() => {
+        if (document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))')) {
+            document.querySelector('.ss-alert-modal-svg-container:not(:has(svg.new))').innerHTML = `<svg class="new" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/></svg>`;
+            if (document.querySelector('.ss-alert-modal-cta-button') && (document.querySelector('.ss-alert-modal-cta-button').href === window.location.href)) document.querySelector('.ss-alert-modal-cta-button').remove();
+            setTimeout(() => {
+                document.querySelector('.alert-badge').classList.add('active');
+            }, 100);
+        };
+        if (!document.querySelector('.ss-alert-modal-svg-container')) clearInterval(changeIconInterval);
+    }, 500);
 });
 
 function scrollToHash() {
