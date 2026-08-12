b0335ccaa17349094c3563a47916d7c7c948d285
Faisal N <contact@faisaln.com>
2026-08-12 18:33:42 -0400

    Bug fixes

--- global.js
+++ global.js
@@ -429,7 +429,7 @@ if (window.location.search.includes('disable')) {
                         pageSectionLi.addEventListener('click', () => {
                             if (window.location.hash === pageSectionLink.getAttribute('href')) scrollToHash();
                         });
-                        pageSection.element.querySelectorAll('span')[pageSection.element.querySelectorAll('span').length - 1].outerHTML = `<a href="#${pageSection.slug}">${pageSection.title}</a>`;
+                        if (pageSection.element.classList.contains('ss-has-bg') && !pageSection.element.querySelector('.ss-component-header-title')) pageSection.element.querySelectorAll('.ss-editor-content *:not(.forceStyle) span')[pageSection.element.querySelectorAll('.ss-editor-content *:not(.forceStyle) span').length - 1].outerHTML = `<a href="#${pageSection.slug}">${pageSection.title}</a>`;
                     };
                     pageSectionsLink.appendChild(pageSectionsDiv);
                     nav.prepend(pageSectionsLink);
